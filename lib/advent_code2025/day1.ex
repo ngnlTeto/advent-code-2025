5705104efb6224c2dc39dc3f395446252f096558
Henry <henry@gedert.de>
2025-12-01 22:33:01 +0100

    Optimized day 1

--- lib/advent_code2025/day1.ex
+++ lib/advent_code2025/day1.ex
@@ -1,98 +1,43 @@
 defmodule AdventCode2025.Day1 do
-  @moduledoc """
-  Placeholder for Day 1 solutions. Replace with your implementation.
-  """
-
   @input_file Path.join(__DIR__, "../../inputs/day1.txt")
   @dial_size 100
   @start_dial 50
 
-  @doc """
-  Run Day 1 solution. Return whatever representation you prefer.
-  """
   def run do
-    %{task1: run_part1(), task2: run_part2()}
+    instructions = File.read!(@input_file) |> parse
+    %{task1: run_part1(instructions), task2: run_part2(instructions)}
   end
 
-  defp run_part1 do
-    instructions = File.read!(@input_file) |> String.split("\n", trim: true)
+  defp parse_line("L" <> rotation), do: -String.to_integer(rotation)
+  defp parse_line("R" <> rotation), do: +String.to_integer(rotation)
 
-    operations =
-      Enum.map(
-        instructions,
-        fn i ->
-          i
-          |> String.replace_prefix("R", "+")
-          |> String.replace_prefix("L", "-")
-          |> Integer.parse()
-          |> elem(0)
-        end
-      )
-
-    result =
-      Enum.reduce(
-        operations,
-        {0, @start_dial},
-        fn x, acc ->
-          newDial = Integer.mod(elem(acc, 1) + x + 10000, @dial_size)
-          newSum = if newDial === 0, do: elem(acc, 0) + 1, else: elem(acc, 0)
-          {newSum, newDial}
-        end
-      )
-
-    elem(result, 0)
+  defp parse(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&parse_line/1)
   end
 
-  defp run_part2 do
-    instructions = File.read!(@input_file) |> String.split("\n", trim: true)
+  defp run_part1(instructions) do
+    {_dial, count} =
+      instructions
+      |> Enum.reduce({@start_dial, 0}, fn x, {curr, count} ->
+        dial = Integer.mod(curr + x, @dial_size)
+        {dial, count + if(dial === 0, do: 1, else: 0)}
+      end)
 
-    operations =
-      Enum.map(
-        instructions,
-        fn i ->
-          i
-          |> String.replace_prefix("R", "+")
-          |> String.replace_prefix("L", "-")
-          |> Integer.parse()
-          |> elem(0)
-        end
-      )
+    count
+  end
 
-    result =
-      Enum.reduce(
-        operations,
-        {0, @start_dial},
-        fn x, acc ->
-          newDial = elem(acc, 1) + x
-          sum = elem(acc, 0)
+  defp run_part2(instructions) do
+    {_dial, count} =
+      instructions
+      |> Enum.reduce({@start_dial, 0}, fn x, {curr, count} ->
+        dial = curr + x
+        incr = abs(div(dial, @dial_size)) + if curr !== 0 && dial <= 0, do: 1, else: 0
+        {Integer.mod(dial, @dial_size), count + incr}
+      end)
 
-          newsum =
-            cond do
-              newDial < 0 ->
-                tempSum = sum + div(newDial, -100)
-
-                if elem(acc, 1) !== 0 do
-                  tempSum + 1
-                else
-                  tempSum
-                end
-
-              99 < newDial ->
-                sum + div(newDial, 100)
-
-              newDial === 0 ->
-                sum + 1
-
-              true ->
-                sum
-            end
-
-          newDial = (newDial + 10000) |> Integer.mod(@dial_size)
-
-          {newsum, newDial}
-        end
-      )
-
-    elem(result, 0)
+    count
   end
 end
