1e086b3366b16fc0c176472961f4a4d46701c173
Henry <henry@gedert.de>
2025-12-01 20:42:28 +0100

    Implemented day 1 task 1

--- lib/advent_code2025/day1.ex
+++ lib/advent_code2025/day1.ex
@@ -10,7 +10,31 @@ defmodule AdventCode2025.Day1 do
   """
   def run do
     input = File.read!(@input_file)
-    # Replace this with actual solution code. Returning a sample result for now.
-    %{message: "Day 1 not implemented", sample: 42}
+    instructions = String.split(input, "\n", trim: true)
+
+    operations =
+      Enum.map(
+        instructions,
+        fn i ->
+          i
+          |> String.replace_prefix("R", "+")
+          |> String.replace_prefix("L", "-")
+          |> Integer.parse()
+          |> elem(0)
+        end
+      )
+
+    result =
+      Enum.reduce(
+        operations,
+        {0, 50},
+        fn x, acc ->
+          newDial = Integer.mod(elem(acc, 1) + x + 10000, 100)
+          newSum = if newDial === 0, do: elem(acc, 0) + 1, else: elem(acc, 0)
+          {newSum, newDial}
+        end
+      )
+
+    %{result: elem(result, 0)}
   end
 end
