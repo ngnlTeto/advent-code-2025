95acb3accd061be59c485fd3994ce39ad81a536d
Henry <henry@gedert.de>
2025-12-07 18:26:43 +0100

    Added tree generator

--- lib/advent_code2025/day7.ex
+++ lib/advent_code2025/day7.ex
@@ -1,7 +1,7 @@
 defmodule AdventCode2025.Day7 do
   require AdventCode2025.Util
 
-  @input_file Path.join(__DIR__, "../../inputs/day7.txt")
+  @input_file Path.join(__DIR__, "../../inputs/day7.test.txt")
 
   def run do
     input = File.read!(@input_file) |> parse
@@ -22,18 +22,62 @@ defmodule AdventCode2025.Day7 do
   end
 
   defp run_part1({splitter, tachyon, height}) do
-    {tachyon_x, _tachyon_y} = tachyon
+    {tachyon_x, _} = tachyon
     beam = MapSet.new([tachyon_x])
     calc_beam(splitter, beam, 1, height - 1)
   end
 
-  defp run_part2(_input) do
-    nil
+  defp run_part2({splitter, tachyon, _}) do
+    first_pos =
+      splitter
+      |> MapSet.filter(fn s -> elem(s, 0) == elem(tachyon, 0) end)
+      |> MapSet.to_list()
+      |> Enum.min_by(&elem(&1, 1))
+
+    base_node = gen_splitter_tree(first_pos, splitter)
+
+    IO.inspect(base_node, limit: :infinity)
+
+    base_node
+  end
+
+  defp gen_splitter_tree(pos, splitter) do
+    next_splitter = find_next_splitter(pos, splitter)
+
+    nodes =
+      next_splitter
+      |> Enum.map(&gen_splitter_tree(&1, splitter))
+
+    %{pos: pos, childs: MapSet.new(nodes)}
+  end
+
+  defp find_next_splitter(pos, splitter) do
+    left_splitter =
+      splitter
+      |> MapSet.filter(fn s -> elem(pos, 0) - 1 == elem(s, 0) end)
+      |> MapSet.filter(fn s -> elem(pos, 1) < elem(s, 1) end)
+      |> MapSet.to_list()
+
+    left =
+      if Enum.empty?(left_splitter),
+        do: [],
+        else: [Enum.min_by(left_splitter, &elem(&1, 1))]
+
+    right_splitter =
+      splitter
+      |> MapSet.filter(fn s -> elem(pos, 0) + 1 == elem(s, 0) end)
+      |> MapSet.filter(fn s -> elem(pos, 1) < elem(s, 1) end)
+      |> MapSet.to_list()
+
+    right =
+      if Enum.empty?(right_splitter),
+        do: [],
+        else: [Enum.min_by(right_splitter, &elem(&1, 1))]
+
+    left ++ right
   end
 
   defp calc_beam(splitter, beams, level, height) do
-    IO.inspect(splitter)
-
     cur_level_splitter =
       splitter
       |> MapSet.filter(fn s -> elem(s, 1) == level end)
